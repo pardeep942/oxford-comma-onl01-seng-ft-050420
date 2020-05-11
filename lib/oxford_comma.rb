9b1b6b5d7f38e6585932b61dd1cb825ece810b35
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 01:38:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
-     if array.length == 1
-      
-   elsif array.length == 2
-      array.join(" and ")
-   else
-      lastStr = array.pop
-      
-      array.join(", ") << ", and #{lastStr}"
-      
-   end
+     ef oxford_comma(array)
+  if array.size == 2
+    array.join(" and ")
+  elsif 2 < array.size
+    last_word = array.pop
+   new_list = array.join(", ")
+    new_list << ", and #{last_word}"
+  else
+    array.join
+  end
 end
 
 
